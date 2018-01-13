9dc8efc4136d1f5763d8ffac1cdfa316ebebcd36
Victor Felix <vfelix82@gmail.com>
2018-01-12 16:24:17 -0800

    fix indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
   $(".form").submit(function(event){
+    ("#question1")alert();
+    event.preventDefault();
     var question1 = parseInt($("#question1").val());
     var question2 = parseInt($("#question2").val());
     var question3 = parseInt($("#question3").val());
     var question4 = parseInt($("#question4").val());
-    var sumAll = question1 + question2 + question3 + question4
-    var userInputName = $("#userName").val();
-    var userInputEmail = $("#userEmail").val();
+    var sumAll = add(#question1 + #question2)
     if (12 === sumAll) {
-      $(".java, .php, .c-sharp").show();
-    }
+      $(".java, .ruby, .c-sharp, .php, .cc").show();
+      $(".none").hide();
+  });
 });
