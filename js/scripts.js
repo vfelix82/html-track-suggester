4be95bc365eabaeea0dea3e195e24e12c70839a1
Victor Felix <vfelix82@gmail.com>
2018-01-16 19:22:32 -0800

    add working javascript functions, add another quetion bar, add reload button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,29 @@
+$(".form-reload").click(function() {
+    location.reload();
+});
+
 $(document).ready(function(){
   $(".form").submit(function(event){
-    ("#question1")alert();
     event.preventDefault();
-    var question1 = parseInt($("#question1").val());
-    var question2 = parseInt($("#question2").val());
-    var question3 = parseInt($("#question3").val());
-    var question4 = parseInt($("#question4").val());
-    var sumAll = add(#question1 + #question2)
-    if (12 === sumAll) {
-      $(".java, .ruby, .c-sharp, .php, .cc").show();
-      $(".none").hide();
+    var answer1 = parseInt($("#question1").val());
+    var answer2 = parseInt($("#question2").val());
+    var answer3 = parseInt($("#question3").val());
+    var answer4 = parseInt($("#question4").val());
+    var answer5 = parseInt($("#question5").val());
+    var answerAll = answer1 + answer2 + answer3 + answer4 + answer5
+    var userrName = $("#userName").val();
+    if (answerAll === 15) {
+      $(".java, .c-sharp, .ruby, .form, h2").hide();
+      $(".php, .css, h5, .form-reload").show();
+      $(".userName").empty().append(userrName + " ");
+    } else if (answerAll >= 10) {
+      $(".php, .c-sharp, .ruby, .form, h2").hide();
+      $(".java, .php, h5, .form-reload").show();
+      $(".userName").empty().append(userrName + " ");
+    } else if (answerAll >= 5) {
+      $(".java, .php, .css, .form, h2").hide();
+      $(".c-sharp, .ruby, h5, .form-reload").show();
+      $(".userName").empty().append(userrName + " ");
+    }
   });
 });
